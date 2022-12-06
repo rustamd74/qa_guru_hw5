4517419b5ac6b441bd26937f1615ce149f47c1e9
Rustam Dzhafarov <rustamd74@gmail.com>
2022-12-06 17:49:45 +0300

    HomeWork5 - working version 2

--- conftest.py
+++ conftest.py
@@ -4,8 +4,9 @@ from selene import command, have
 
 @pytest.fixture(scope="function", autouse=True)
 def open_browser():
-    browser.open('https://demoqa.com/automation-practice-form').driver.maximize_window()
-    browser.all('[id^=google_ads_iframe_][id$=__container__]').should(have.size_greater_than_or_equal(3)).perform(command.js.remove)
-    b = browser.open('https://demoqa.com/automation-practice-form')
-    yield b
+    browser.config.base_url = 'https://demoqa.com'
+    browser.config.window_width = 1600
+    browser.config.window_height = 900
+    #browser.all('[id^=google_ads_iframe_][id$=__container__]').should(have.size_greater_than_or_equal(3)).perform(command.js.remove)
+    yield
     browser.quit()
--- test_demoqa_filling_out_form.py
+++ test_demoqa_filling_out_form.py
@@ -4,6 +4,7 @@ import os
 
 
 def test_filling_form(open_browser):
+    browser.open('/automation-practice-form')
     browser.element('#firstName').should(be.blank).type('John')
     browser.element('#lastName').should(be.blank).type('Doe')
     browser.element('#userEmail').should(be.blank).type('johndoe@gmail.com')
@@ -13,7 +14,7 @@ def test_filling_form(open_browser):
     browser.element('.react-datepicker__month-select>[value="4"').click()
     browser.element('.react-datepicker__year-select>[value="1901"]').click()
     browser.element('.react-datepicker__day--004').click()
-    browser.element('#subjectsInput').should(be.blank).type('QA Automation')
+    browser.element('#subjectsInput').type('Computer Science').press_enter()
     browser.element('[for="hobbies-checkbox-1"]').click()
     browser.element('[for="hobbies-checkbox-3"]').click()
     browser.element('#uploadPicture').set_value(os.path.abspath('file/python_label.png'))
@@ -24,5 +25,5 @@ def test_filling_form(open_browser):
 
     browser.element('#example-modal-sizes-title-lg').should(have.text('Thanks for submitting the form'))
     browser.element('.table').should(have.text(
-        'John Doe'and 'johndoe@gmail.com'and 'Male'and '2223331110'and '04 April,1901'and ''and 'Sports,Music'and
+        'John Doe'and 'johndoe@gmail.com'and 'Male'and '2223331110'and '04 April,1901'and 'Computer Science'and 'Sports,Music'and
         'python_label.png'and '221b, Baker street'and 'Uttar Pradesh Lucknow'))
